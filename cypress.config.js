fe8677cb6ee2fe8a98830c004aee724af982d594
Borysov Kostyantyn <koss199710@gmail.com>
2022-09-27 19:23:17 +0300

    Make changes in cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'ja9b7k',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+
+
